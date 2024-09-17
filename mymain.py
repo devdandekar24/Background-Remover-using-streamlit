01e971305a9820eae9fff3d7238e2871708a361e
devdandekar24 <dandekardev365@gmail.com>
2024-09-17 16:59:02 +0530

    Added a camera input

--- mymain.py
+++ mymain.py
@@ -6,7 +6,7 @@ import base64
 
 st.set_page_config(layout="wide",page_title="Image Bg remover")
 
-st.write("## Remove Background from your image ")
+st.write("## :orange[Remove Background from your image] ")
 st.write(":cat: Try uploading image to view their backgrounds removed magically.Full Quality images.")
 
 st.sidebar.write("## upload and download :gear:")
@@ -32,13 +32,19 @@ def fix_img(upload):
     col2.write("Bgfree Image :camera:")
     col2.image(fixed)
     st.sidebar.markdown("\n")
-    st.sidebar.download_button("Download bgfree image",convert_img(fixed),"fixed.png","image.png")
+    st.sidebar.download_button("Download bgfree image",convert_img(fixed),"bgfree.png","image.png")
 
 
 my_upload=st.sidebar.file_uploader("Upload an image", type=["png","jpg","jpeg"])
+camera_photo=st.sidebar.camera_input("Take a live photo")
 
 if my_upload is not None:
     if my_upload.size > MAX_FILE_SIZE:
         st.error("The uploaded file is too large. Upload file smaller than 20MB ")
     else:
         fix_img(upload=my_upload)
+else:
+    if camera_photo is not None:
+        fix_img(upload=camera_photo)
+    else:
+        st.write("Add your Image using camera or upload option")
